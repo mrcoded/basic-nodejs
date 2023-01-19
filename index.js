33a195e5dbe620e5073cd49c021a1cea2c7acd47
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-19 01:32:23 +0100

    feat: added the genre PuT and POST endpoint

--- index.js
+++ index.js
@@ -20,3 +20,76 @@ app.get("/api/genres/:id", (req, res) => {
     if(!genre) return res.status(404).send("Genre not found");
     else return res.send(genre);
 }); //get unique genre
+
+//handling POST request
+app.post("/api/genres/", (req, res) => {
+    //validate request
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+        const value = {
+            name: ""
+        }
+    //input validation with Joi
+    const {error} = schema.validate(req.body, value);
+  
+    //if invalid return 400
+    if (error) {
+    //400 Bad request
+    res.status(400).send(error.details[0].message);
+    return;
+    }   
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+}); //post to movie genre
+
+//handling PUT request
+app.post("/api/genres/:id", (req, res) => {
+    const genre = genres.find(genre => genre.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("Genre not found");
+
+    //validate request
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+        const value = {
+            name: ""
+        }
+    //input validation with Joi
+    const {error} = schema.validate(req.body, value);
+  
+    //if invalid return 400
+    if (error) {
+    //400 Bad request
+    res.status(400).send(error.details[0].message);
+    return;
+    }   
+
+}); //post to movie genre
+
+//handling DELETE request
+app.post("/api/genres/:id", (req, res) => {
+    const genre = genres.find(genre => genre.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("Genre not found");
+
+    //validate request
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+        const value = {
+            name: ""
+        }
+    //input validation with Joi
+    const {error} = schema.validate(req.body, value);
+  
+    //if invalid return 400
+    if (error) {
+    //400 Bad request
+    res.status(400).send(error.details[0].message);
+    return;
+    }   
+
+}); //post to movie genre
