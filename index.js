a6156c9a5a87640899a417be5738fc06a7b2a143
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-19 01:19:37 +0100

    feat: added the genre GET endpoint

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const app = express();
 app.use(express.json());
 
 const genres = [
-    {id: 1,  movie: "Game of thrones"},
-    {id: 2,  movie: "How to get away with murder"},
-    {id: 3,  movie: "On my block"}
+    {id: 1,  name: "Game of thrones"},
+    {id: 2,  name: "How to get away with murder"},
+    {id: 3,  name: "On my block"}
 ];
 
+app.get("/api/genres/:id", (res, req) => {
+    res.send(genre);
+});
+
+//handling GET request
+app.get("/api/genres/:id", (req, res) => {
+    const genre = genres.find(genre => genre.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("Genre not found");
+    else return res.send(genre);
+}); //get unique genre
