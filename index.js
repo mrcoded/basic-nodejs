648be1a4bc28d3af5d0b0a9c2e31aac5f1f32397
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-19 01:14:12 +0100

    created a genre array for movies object

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+
+const genres = [
+    {id: 1,  movie: "Game of thrones"},
+    {id: 2,  movie: "How to get away with murder"},
+    {id: 3,  movie: "On my block"}
+];
+
