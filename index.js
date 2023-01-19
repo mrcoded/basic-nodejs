f51894d32697da0f1862c020fad47571b907247e
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-19 01:36:48 +0100

    feat: added the genre DELETE endpoint

--- index.js
+++ index.js
@@ -75,21 +75,8 @@ app.post("/api/genres/:id", (req, res) => {
     const genre = genres.find(genre => genre.id === parseInt(req.params.id));
     if(!genre) return res.status(404).send("Genre not found");
 
-    //validate request
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-        const value = {
-            name: ""
-        }
-    //input validation with Joi
-    const {error} = schema.validate(req.body, value);
-  
-    //if invalid return 400
-    if (error) {
-    //400 Bad request
-    res.status(400).send(error.details[0].message);
-    return;
-    }   
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
 
-}); //post to movie genre
+    res.send(genre);
+}); //delete to movie genre
