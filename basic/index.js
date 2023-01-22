bffafb245222a2fbab293e648ad7585d0a6689c1
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-22 23:54:22 +0100

    installed mongoose and setup connection with the mongoDB

--- basic/index.js
+++ basic/index.js
@@ -1,7 +1,13 @@
+const mongoose = require("mongoose");
 const Joi = require("joi");
 const express = require("express");
 const genres = require("./routes/genres");
 const app = express();
+
+
+mongoose.connect("mongodb://localhost/vidly")
+    .then(() => console.log("Connected to the database..."))
+    .catch(err => console.log("Could not connect to db...", err));
 
 app.use(express.json());
 
