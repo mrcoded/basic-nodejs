1969e9d278e735d5ec3c528458e00a96794b16be
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-23 01:35:48 +0100

    chore: renamed 'Course' to Genre in PUT and DELETE endpoint

--- vidly-db/routes/genres.js
+++ vidly-db/routes/genres.js
@@ -14,23 +14,21 @@ const Genre = mongoose.model("Genre", new mongoose.Schema({
 
 //handling GET request
 router.get("/", async (req, res) => {
-    const genres = await Genre.find().sort("name");
-    res.send(genres);
+    const genre = await Genre.find().sort("name");
+    res.send(genre);
 });
 
-
 router.get("/:id", async (req, res) => {
-    const genres = await Genre.find().sort("name");
-    res.send(genres);
-    if(!genre) return res.status(404).send("Genre not found");
-    else return res.send(genre);
-}); //get unique genre
+    const genre = await Genre.find().sort("name");
+    res.send(genre);
+    if(!genre) return res.status(404).send("Genre not found...");
+    res.send(genre);
+}); //get unique movie genre
 
 //handling POST request
 router.post("/", async (req, res) => {
     //validate request
     const {error} = validateGenre(req.body);
-
     //if invalid return 400
     if (error) {
     //400 Bad request
@@ -54,7 +52,7 @@ router.put("/:id", async (req, res) => {
     return res.status(400).send(error.details[0].message);
     }   
 
-    const genre = await Course.findByIdAndUpdate(req.params.id, 
+    const genre = await Genre.findByIdAndUpdate(req.params.id, 
         { name: req.body.name }, {
         new: true
     });
@@ -66,7 +64,7 @@ router.put("/:id", async (req, res) => {
 
 //handling DELETE request
 router.post("/:id", async (req, res) => {
-    const genre = await Course.findByIdAndRemove(req.params.id, {
+    const genre = await Genre.findByIdAndRemove(req.params.id, {
         new: true
     });
 
