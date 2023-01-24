38480d2e2f0a32e36c070ce3c4599ed4a18e0786
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-24 13:24:04 +0100

    feat: created POST endpoint for movie

--- vidly-db/routes/movies.js
+++ vidly-db/routes/movies.js
@@ -1,4 +1,4 @@
-const {validateGenre, Movie} = require("../models/movie")
+const {validateMoview, Movie} = require("../models/movie")
 const express = require("express");
 const { required } = require("joi");
 const {Genre} = require("../models/genres");
@@ -7,6 +7,33 @@ const router = express.Router();
 
 //handling GET request
 router.get("/", async (req, res) => {
-    const movie = await Movie.find().sort("name");
+    const movies = await Movie.find().sort("name");
+    res.send(movies);
+});
+
+//handling POST request
+router.post("/", async (req, res) => {
+    //validate request
+    const {error} = validateMoview(req.body);
+    //if invalid return 400
+    if (error) {
+    //400 Bad request
+    return res.status(400).send(error.details[0].message);
+    }   
+
+    const genre = await Genre.findById(req.body.genreId); //find by genreId
+    if(!genre) return res.status(404).send("Genre not found...");
+
+    let movie = new Movie({ 
+        title: req.body.title,
+        genre: {
+            _id: genre._id,
+            name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+    });
+    movie = await movie.save();
+
     res.send(movie);
 });
