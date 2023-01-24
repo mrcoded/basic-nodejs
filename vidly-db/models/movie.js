dbe5fca930af65c9571701bca0280e421054dd84
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-24 12:37:49 +0100

    feat: created the movie model

---
+++ vidly-db/models/movie.js
@@ -0,0 +1,27 @@
+//movies model is utilizing the hybrid approach for relationship
+
+const mongoose = require("mongoose");
+
+const Movie = mongoose.model("Movie", new mongoose.Schema({
+    title: { 
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        trim: true
+    }, //remove new from mongoose because mongoose is not a class, but a mthod
+    genre: {
+        type: genreSchema,
+        required: true
+    },
+    numberInStock: {
+        type: Number,
+        required: true
+    },
+    dailyRentalRate: {
+        type: Number,
+        required: true
+    }
+}));
+
+exports.Genre = Movie;
