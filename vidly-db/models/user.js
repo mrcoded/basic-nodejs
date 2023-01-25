415a518c1bc7d52552b098aca54246ff289a7116
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-25 01:26:41 +0100

    feat: created a validateUser function in users model

--- vidly-db/models/user.js
+++ vidly-db/models/user.js
@@ -22,5 +22,19 @@ const Genre = mongoose.model("Genre", new mongoose.Schema({
     } //remove new from mongoose because mongoose is not a class, but a mthod
 }));
 
-exports.Genre = Genre;
-exports.genreSchema = genreSchema;
+const Joi = require("joi"); //Joi cos it returns a class
+
+function validateUser(user) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(1024).required(),
+    });
+        const value = { name: "" }
+    //input validation with Joi
+    return schema.validate(user, value);
+}
+
+
+exports.validateUser = validateUser;
+exports.User = User;
