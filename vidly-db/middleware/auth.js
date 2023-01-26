671debf9db1798546a5e7095e41a4c2e5d1cc944
codedlibra <adeola.ogundijo@yahoo.com>
2023-01-26 03:53:03 +0100

    feat: added try and catch to verification of login token in the uath middleware

--- vidly-db/middleware/auth.js
+++ vidly-db/middleware/auth.js
@@ -5,6 +5,12 @@ module.exports = function auth(req, res, next) {
     const token = req.header("x-auth-token");
     if(!token) return res.status(401).send("Access denied. No token provided.")
 
-    jwt.verify(token, config.get("jwtPrivateKey"));
-    next()
+    try {
+        const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+        req.user = decoded;
+        next()
+    }
+    catch (ex) {
+        res.status(400).send("Invalid Login attempt")
+    }
 }
